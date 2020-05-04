afb1befc1f96607cec1af4ea4738f04f97a342fd
HeeBeom Wang <60314278+kt3472@users.noreply.github.com>
2020-05-04 16:38:44 +0900

    Update NN_0504.py

--- neural network/NN_0504.py
+++ neural network/NN_0504.py
@@ -1,7 +1,7 @@
 import numpy as np
 from make_dataset import load_data_spiral
 from optimizer import SGD
-from layers import Affine, SoftmaxWithLoss
+from layers import Affine, SoftmaxWithLoss, Sigmoid
 import matplotlib.pyplot as plt
 
 max_epoch = 300
@@ -109,7 +109,7 @@ Z = predict_cls.reshape(xx.shape)
 plt.contourf(xx, yy, Z)
 plt.axis('off')
 # 데이터점 플롯
-x, t = load_data()
+x, t = load_data_spiral()
 N = 100
 CLS_NUM = 3
 markers = ['o', 'x', '^']
