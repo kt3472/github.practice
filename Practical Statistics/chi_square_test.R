d61849249d929c01999e3653e38cab5a501e1202
HeeBeom Wang <60314278+kt3472@users.noreply.github.com>
2020-07-20 23:48:53 +0900

    Update chi_square_test.R

--- Practical Statistics/chi_square_test.R
+++ Practical Statistics/chi_square_test.R
@@ -13,3 +13,28 @@ dimnames(clicks) <- list(unique(click_rate$Headline), unique(click_rate$Click))
 chisq.test(clicks, simulate.p.value=TRUE)
 
 chisq.test(clicks, simulate.p.value=FALSE)
+
+x <- seq(1, 30, length=100)
+chi <- data.frame(df = factor(rep(c(1, 2, 5, 10), rep(100, 4))),
+                  x = rep(x, 4),
+                  p = c(dchisq(x, 1), dchisq(x, 2), dchisq(x, 5), dchisq(x, 20)))
+
+ggplot(chi, aes(x=x, y=p)) +
+  geom_line(aes(linetype=df)) +
+  theme_bw() +
+  labs(x='', y='')
+
+dev.off()
+
+## Fishers exact test
+fisher.test(clicks)
+
+imanishi$Digit <- factor(imanishi$Digit)
+ggplot(imanishi, aes(x=Digit, y=Frequency)) +
+  geom_bar(stat="identity") +
+  theme_bw()
+  
+dev.off()
+
+
+
